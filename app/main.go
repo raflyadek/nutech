58e81c8635ac7ad0436f890f2ce35b5a1ba87b67
rafly <raflyade9@gmail.com>
2026-01-11 14:49:27 +0700

    feat: user feature

--- app/main.go
+++ app/main.go
@@ -3,18 +3,36 @@ package main
 import (
 	"log"
 	"nutech-test/config"
+	"nutech-test/internal/controller"
+	"nutech-test/internal/repository"
+	"nutech-test/internal/service"
 	"os"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo"
 )
 
 func main() {
-	_, err := config.ConnectionDb()
+	db, err := config.ConnectionDb()
 	if err != nil {
 		log.Printf("error connect to db %s", err)
 	}
+	//close db connection if exit from main
+	defer db.Close()
 
+	
 	//dependency injection
+	//validator
+	validate := validator.New()
+
+	//repository
+	userRepository := repository.NewUserRepository(db)
+
+	//service
+	userService := service.NewUserService(userRepository)
+
+	//controller
+	userController := controller.NewUserController(userService, validate)
 
 	//http routing using echo
 	e := echo.New()
@@ -26,8 +44,13 @@ func main() {
 	}
 
 	//route
+	e.POST("/registration", userController.CreateUser)
+	e.POST("/login", userController.LoginUser)
 
 	//protected route
+	e.GET("/profile", userController.GetUserProfileByEmail)
+	e.GET("/profile/update", userController.UpdateUserByEmail)
+	e.GET("/profile/image", userController.UpdateUserImageByEmail)
 
 	//start server
 	if err := e.Start(":" + port); err != nil {
